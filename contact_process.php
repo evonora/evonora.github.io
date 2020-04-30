2a4f3213071f379219fb6ba58e900b3bbc6f2642
evonora <64470550+evonora@users.noreply.github.com>
2020-04-30 13:02:27 +0200

    Contact Form Update

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "spn10@spondonit.com";
+    $to = "wildraingames@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
